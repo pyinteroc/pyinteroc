3f191103601fd7be4c461cac38ba0b02d2962526
BridLeiva <yk50tcj5@duck.com>
2024-05-23 19:24:32 -0600

    specifying local branch when publishing changes

--- ci/push_to_public_repos.sh
+++ ci/push_to_public_repos.sh
@@ -3,8 +3,8 @@
 ### This assumes we are working in the trunk branch
 
 ### Deploy to origin first
-git pull origin master
-git push origin master
+git pull origin master:trunk
+git push origin trunk:master
 
 ### deploy to github
-git push github main
+git push github trunk:main
