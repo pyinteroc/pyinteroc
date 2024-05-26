38ba0dcb8d5c850efab75392e3046709dc317e81
BridLeiva <yk50tcj5@duck.com>
2024-05-26 01:31:18 -0600

    cleaning up the flake code

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     roc.url = "github:roc-lang/roc";
-
   };
 
   outputs = { self, nixpkgs, roc, ... }@inputs:
@@ -13,54 +12,38 @@
       pkgs = nixpkgs.legacyPackages.${system};
       rocPkgs = roc.packages.${system};
 
-
-      # Define packages as an attribute set
       packageSet = with pkgs; {
-        inherit entr;
-        inherit zig;
-
-        ### add roc command
-        roc = rocPkgs.cli;
-        
-        
-        ### add deployRocNightly script and utilities
         inherit curl;
+        inherit entr;
+
+        inherit zig;
+        roc = rocPkgs.cli;
         
         deployRocNightly = 
           writeShellScriptBin "deployRocNightly" ./ci/deploy_roc_nightly.sh ;
         
         publish = 
           writeShellScriptBin "publish" ./ci/push_to_public_repos.sh ;
-        
       };
 
     in rec {
       defaultShell = pkgs.mkShell {
         buildInputs = with packageSet; [
-
-          ### utilities
           entr
           curl
           deployRocNightly
           publish
-          
-          
         ];
-
       };
       
-      ### defining available shells
       devShells.${system} = {
         default = defaultShell;
 
         allPackages = pkgs.mkShell {
-          # Convert the attribute set to a list for buildInputs
           buildInputs = builtins.attrValues packageSet;
-          shellHook = ''alias roc="./roc"'';
         };
       };
       
-      ### Assign the attribute set directly to packages
       packages = packageSet;
 
     };
