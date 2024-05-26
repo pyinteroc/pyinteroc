5caf4749fd5e0273b98852b1d5a9e4498c0be62a
BridLeiva <yk50tcj5@duck.com>
2024-05-26 01:23:15 -0600

    updating flake

--- flake.nix
+++ flake.nix
@@ -3,68 +3,65 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     roc.url = "github:roc-lang/roc";
 
   };
 
-  outputs = { self, nixpkgs, flake-utils, roc, ... }@inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        rocPkgs = roc.packages.${system};
+  outputs = { self, nixpkgs, roc, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      rocPkgs = roc.packages.${system};
 
 
-        # Define packages as an attribute set
-        packageSet = with pkgs; {
-          inherit entr;
-          inherit zig;
+      # Define packages as an attribute set
+      packageSet = with pkgs; {
+        inherit entr;
+        inherit zig;
 
-          ### add roc command
-          roc = rocPkgs.cli;
-          
-          
-          ### add deployRocNightly script and utilities
-          inherit curl;
-          
-          deployRocNightly = 
-            writeShellScriptBin "deployRocNightly" ./ci/deploy_roc_nightly.sh ;
-          
-          publish = 
-            writeShellScriptBin "publish" ./ci/push_to_public_repos.sh ;
-          
-        };
-
-      in rec {
-        defaultShell = pkgs.mkShell {
-          buildInputs = with packageSet; [
-
-            ### utilities
-            entr
-            curl
-            deployRocNightly
-            publish
-            
-            
-          ];
-
-        };
+        ### add roc command
+        roc = rocPkgs.cli;
         
-        ### defining available shells
-        devShells = {
-          default = defaultShell;
-
-          allPackages = pkgs.mkShell {
-            # Convert the attribute set to a list for buildInputs
-            buildInputs = builtins.attrValues packageSet;
-            shellHook = ''alias roc="./roc"'';
-          };
-        };
         
-        ### Assign the attribute set directly to packages
-        packages = packageSet;
+        ### add deployRocNightly script and utilities
+        inherit curl;
+        
+        deployRocNightly = 
+          writeShellScriptBin "deployRocNightly" ./ci/deploy_roc_nightly.sh ;
+        
+        publish = 
+          writeShellScriptBin "publish" ./ci/push_to_public_repos.sh ;
+        
+      };
 
-        defaultPackage = self.packages.${system}.deployRocNightly;
-      }
-    );
+    in rec {
+      defaultShell = pkgs.mkShell {
+        buildInputs = with packageSet; [
+
+          ### utilities
+          entr
+          curl
+          deployRocNightly
+          publish
+          
+          
+        ];
+
+      };
+      
+      ### defining available shells
+      devShells.${system} = {
+        default = defaultShell;
+
+        allPackages = pkgs.mkShell {
+          # Convert the attribute set to a list for buildInputs
+          buildInputs = builtins.attrValues packageSet;
+          shellHook = ''alias roc="./roc"'';
+        };
+      };
+      
+      ### Assign the attribute set directly to packages
+      packages = packageSet;
+
+    };
 }
