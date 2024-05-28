0f81c68dd80c9933a8af1e477049072d804e52f8
BridLeiva <yk50tcj5@duck.com>
2024-05-28 13:00:39 -0600

    some cleanup

--- roc.py
+++ roc.py
@@ -3,16 +3,15 @@ import importlib.resources as pkg_resources
 
 ffi = FFI()
 
-# Assuming you know the function signatures you want to call
 ffi.cdef("""
     int main();
 """)
 
-# Load the shared library
+# Loading the shared library
 lib_path = pkg_resources.files("lib")\
     .joinpath("libhost.so")
+
+### Calling the shared library function
 roc_lib = ffi.dlopen(str(lib_path))
 
-# Now you can call functions from the library, e.g., `lib.add(2, 3)`
 result = roc_lib.main()
-# print(result)
